59c0f6bc0ccc88bb27a0bc8dc2032fcd49d16494
toosyou <toosyou70207@gmail.com>
2017-05-24 03:08:46 +0800

    add tpm support, install from wget

--- install.sh
+++ install.sh
@@ -2,36 +2,49 @@
 
 # install necessary packages
 if [ "$(uname -s)" = "Linux" ]; then
+    echo $(uname -s)
     sudo apt-get update
     sudo apt-get --yes --force-yes -f -m install zsh mosh vim tmux bc
     sudo apt-get --yes --force-yes -f -m git wget python3-dev python3-pip
-elif [ "$(uname -s)" = "Darwin" ]; then
+elif [ "$(uname -s)" = "Darwin" ]; then # mac
     echo $(uname -s)
-    echo "This OS is not supported yet!"
-    exit 0
+    # check if brew exists. If not, install it
+    hash brew 2>/dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install wget zsh mosh tmux
+    brew install git python3-dev python3-pip
 else
     echo $(uname -s)
     echo "This OS is not supported yet!"
     exit 0
 fi
 
-# install thefuck
-pip3 install --user thefuck
+# git clone every thing
+CLONE_PATH=$HOME/myconfig
+git clone https://github.com/toosyou/myconfig $CLONE_PATH
+
+# install thefuck if not exists
+hash thefuck 2>/dev/null || pip3 install --user thefuck
 
 # link tmux configure
 if [ $( echo "$(tmux -V| cut -d" " -f 2| tr -d $'\n'| tr -d $'\r') <= 2.3 " | bc -l ) -eq 1 ];then
-    ln -sf `pwd`/tmux19.conf ~/.tmux.conf
+    ln -sf $CLONE_PATH/tmux19.conf ~/.tmux.conf
 else
-    ln -sf `pwd`/tmux.conf ~/.tmux.conf
+    # install tmux-package-manager(TPM)
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || git -C ~/.tmux/plugins/tpm pull
+    ln -sf $CLONE_PATH/tmux.conf ~/.tmux.conf
+    # install packages
+    ~/.tmux/plugins/tpm/scripts/install_plugins.sh
 fi
 
 # get awsome vim configure
-git clone https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+if [ -e ~/.vim_runtime/. ]; then # install only once
+    git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+    sh ~/.vim_runtime/install_awesome_vimrc.sh
+fi
 
 # install vundle and append vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -sf `pwd`/vundle.vim ~/.vundle.vim
+ln -sf $CLONE_PATH/vundle.vim ~/.vundle.vim
 sed -i '1isource ~/.vundle.vim\n' ~/.vimrc
 vim +PluginInstall +qall # install plugins
 
