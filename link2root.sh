941b1a3dea050c33ee3cb06472aec6bccbb82dc6
toosyou <toosyou.tw@gmail.com>
2017-05-23 14:40:52 +0000

    add link2root for root config

---
+++ link2root.sh
@@ -0,0 +1,9 @@
+MYHOME=`pwd`/..
+ln -sf $MYHOME/.oh-my-zsh             /root/.oh-my-zsh
+ln -sf $MYHOME/.zshrc                 /root/.zshrc
+ln -sf $MYHOME/.vim                   /root/.vim
+ln -sf $MYHOME/.vimrc                 /root/.vimrc
+ln -sf $MYHOME/myconfig/vundle.vim    /root/.vundle.vim
+ln -sf $MYHOME/myconfig/tmux.conf     /root/.tmux.conf
+ln -sf $MYHOME/.vim_runtime           /root/.vim_runtime
+
